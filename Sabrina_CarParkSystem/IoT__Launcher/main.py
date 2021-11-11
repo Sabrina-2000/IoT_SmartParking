a309203ba5d462e548f0698435905e0aee123781
Sabrina-2000 <71062682+Sabrina-2000@users.noreply.github.com>
2021-11-11 19:19:45 +0800

    Add files via upload

--- Sabrina_CarParkSystem/IoT__Launcher/main.py
+++ Sabrina_CarParkSystem/IoT__Launcher/main.py
@@ -10,6 +10,7 @@ import pytesseract
 import paho.mqtt.client as mqtt
 import paho.mqtt.publish as publish
 import smtplib
+import MySQLdb
 from PIL import Image
 from picamera.array import PiRGBArray
 from picamera import PiCamera
@@ -20,17 +21,76 @@ rawCapture = PiRGBArray(camera, size=(640, 480))
 server=smtplib.SMTP('smtp.gmail.com',587)
 server.starttls()
 server.login('iotsmartparking2021@gmail.com', 'smartparking2021')
-
 device = '/dev/ttyUSB0'
 arduino = serial.Serial(device, 9600)
 display = drivers.Lcd()
-action = "0"
+global count, available, slots
+action = 0
 count = 0
 available = 3
 slots = ["-","-","-"]
 plate = ""
 b2status = 0
+s1status = "-"
+s2status = "-"
 emailSend = False
+added = False
+        
+def leaved(plate):
+    global count, available, slots
+    if(slots[0] == plate):
+        slots[0] = "-"
+        available += 1
+        count -= 1
+        display.lcd_display_string("Leaved:", 1)
+        display.lcd_display_string(str(plate), 1)
+        return
+    if(slots[1] == plate):
+        slots[1] = "-"
+        available += 1
+        count -= 1
+        display.lcd_display_string("Leaved:", 1)
+        display.lcd_display_string(str(plate), 1)
+        return
+    if(slots[2] == plate):
+        slots[2] = "-"
+        available += 1
+        count -= 1
+        display.lcd_display_string("Leaved:", 1)
+        display.lcd_display_string(str(plate), 1)
+        return
+    if(slots[0] != "-"):
+        booked = slots[0]
+        if(booked[:-1] == plate):
+            slots[0] = plate
+            display.lcd_display_string("Slot:A1", 1)
+            display.lcd_display_string(plate, 2)
+            return
+    if(slots[1] != "-"):
+        booked = slots[1]
+        if(booked[:-1] == plate):
+            slots[1] = plate
+            display.lcd_display_string("Slot:A2", 1)
+            display.lcd_display_string(plate, 2)
+            return
+    
+def come(plate):
+    global count, available, slots
+    if(slots[0] == "-"):
+        slots[0] = plate
+        display.lcd_display_string("Slot:A1", 1)
+        available -= 1
+        count += 1
+        display.lcd_display_string(plate, 2)
+        return
+        
+    if(slots[1] == "-"):
+        slots[1] = plate
+        display.lcd_display_string("Slot:A2", 1)
+        available -= 1
+        count += 1
+        display.lcd_display_string(plate, 2)
+        return
 
 def capture():
     for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
@@ -69,8 +129,16 @@ def capture():
              (bottomx, bottomy) = (np.max(x), np.max(y))
              Cropped = gray[topx:bottomx+1, topy:bottomy+1]
              text = pytesseract.image_to_string(Cropped, config='--psm 11')
-             print("Detected Number is:",text[:-2])
              plate = text[:-2]
+             plate = plate.replace(" ","")
+             plate = plate.replace("'","")
+             plate = plate.replace("(","")
+             plate = plate.replace(")","")
+             plate = plate.replace(",","")
+             plate = plate.replace(".","")
+             plate = plate.replace(":","")
+             plate = plate.replace(";","")
+             print("Detected Number is:",plate)
              cv2.imshow("Frame", image)
              cv2.imshow('Cropped',Cropped)
              time.sleep(2)
@@ -78,10 +146,56 @@ def capture():
     cv2.destroyAllWindows()
     return plate
 
+dbConn = MySQLdb.connect("localhost","pi","","parkingSystem_db") or dle("Could not connect to database") 
+print(dbConn)
+with dbConn:
+    cursor = dbConn.cursor()
+    cursor.execute("INSERT INTO record (Counts, Availables, SlotA1, SlotA2, SlotB1) VALUES ('%s','%s','%s','%s','%s')" %(count, available, slots[0], slots[1], slots[2])) 
+    dbConn.commit()
+    cursor.close()
 
 while True:
+    dbConn = MySQLdb.connect("localhost","pi","","parkingSystem_db") or dle("Could not connect to database") 
+    with dbConn:
+        cursor = dbConn.cursor()
+        cursor.execute("SELECT * FROM record")
+        dbConn.commit()
+        for Id, Counts, Availables, SlotA1, SlotA2, SlotB1 in cursor:
+            count = int(Counts)
+            available = int(Availables)
+            slots[0] = SlotA1
+            slots[1] = SlotA2
+            slots[2] = SlotB1
+        cursor.close()
+    
+    print("count: " + str(count))
+    print("available: " + str(available))
+    print(slots[0])
+    print(slots[1])
+    
     display.lcd_display_string("Available:" + str(available), 1)
     while(arduino.in_waiting == 0):
+        dbConn = MySQLdb.connect("localhost","pi","","parkingSystem_db") or dle("Could not connect to database") 
+        with dbConn:
+            cursor = dbConn.cursor()
+            cursor.execute("SELECT * FROM record")
+            dbConn.commit()
+            for Id, Counts, Availables, SlotA1, SlotA2, SlotB1 in cursor:
+                count = int(Counts)
+                available = int(Availables)
+                slots[0] = SlotA1
+                slots[1] = SlotA2
+                slots[2] = SlotB1
+            cursor.close()
+        display.lcd_display_string("Available:" + str(available), 1)
+        
+        publish.single(topic="v1/devices/me/telemetry", payload='{"value":' + str(slots[2]) +'}', hostname="thingsboard.cloud", auth = {'username':"nXNiSJDI52nOQqiTy6F0", 'password':""})
+        publish.single(topic="v1/devices/me/telemetry", payload='{"value":' + str(slots[0]) +'}', hostname="thingsboard.cloud", auth = {'username':"4Zdi7c8BlbQB7nL1fLpq", 'password':""})
+        publish.single(topic="v1/devices/me/telemetry", payload='{"status":' + str(s1status) +'}', hostname="thingsboard.cloud", auth = {'username':"4Zdi7c8BlbQB7nL1fLpq", 'password':""})
+        publish.single(topic="v1/devices/me/telemetry", payload='{"value":' + str(slots[1]) +'}', hostname="thingsboard.cloud", auth = {'username':"yUYqesVqB0GBThmr1U4h", 'password':""})
+        publish.single(topic="v1/devices/me/telemetry", payload='{"status":' + str(s2status) +'}', hostname="thingsboard.cloud", auth = {'username':"yUYqesVqB0GBThmr1U4h", 'password':""})
+        publish.single(topic="v1/devices/me/telemetry", payload='{"value":' + str(available) +'}', hostname="thingsboard.cloud", auth = {'username':"l2W9Yt6qDEhBoTVj1FVB", 'password':""})
+        publish.single(topic="v1/devices/me/telemetry", payload='{"value":' + str(count) +'}', hostname="thingsboard.cloud", auth = {'username':"xHyIFxSBeq87pbF3671m", 'password':""})
         pass
 
     line = arduino.readline()
@@ -105,102 +219,93 @@ while True:
         data = data.decode('UTF-8')
         print(data)
         b2status = int(data)
-          
     publish.single(topic="v1/devices/me/telemetry", payload='{"value":' + str(available) +'}', hostname="thingsboard.cloud", auth = {'username':"l2W9Yt6qDEhBoTVj1FVB", 'password':""})
     publish.single(topic="v1/devices/me/telemetry", payload='{"value":' + str(count) +'}', hostname="thingsboard.cloud", auth = {'username':"xHyIFxSBeq87pbF3671m", 'password':""})
+    
+    data = line[0:5]
+    data = data.decode('UTF-8')
+
+    print(data)
+    if(data == "Slot1"):
+        data = line[7:]
+        data = data.decode('UTF-8')
+        data = data.replace("\r\n", "")
+        print(data)
+        s1status = data
+        publish.single(topic="v1/devices/me/telemetry", payload='{"status":' + str(s1status) +'}', hostname="thingsboard.cloud", auth = {'username':"4Zdi7c8BlbQB7nL1fLpq", 'password':""})
+  
+    if(data == "Slot2"):
+        data = line[7:]
+        data = data.decode('UTF-8')
+        data = data.replace("\r\n", "")
+        print(data)
+        s2status = data
+        publish.single(topic="v1/devices/me/telemetry", payload='{"status":' + str(s2status) +'}', hostname="thingsboard.cloud", auth = {'username':"yUYqesVqB0GBThmr1U4h", 'password':""})
+        
+    dbConn = MySQLdb.connect("localhost","pi","","parkingSystem_db") or dle("Could not connect to database") 
+    with dbConn:
+        cursor = dbConn.cursor()
+        cursor.execute("SELECT * FROM record")
+        dbConn.commit()
+        for Id, Counts, Availables, SlotA1, SlotA2, SlotB1 in cursor:
+            count = int(Counts)
+            available = int(Availables)
+            slots[0] = SlotA1
+            slots[1] = SlotA2
+            slots[2] = SlotB1
+        cursor.close()
     
     #action = "1"
     if(int(action) == 1):
         plate = capture()
-        plate = plate.replace(" ","")
-        
         display.lcd_clear()
-        if(slots[0] == plate):
-            slots[0] = "-"
-            available += 1
-            count -= 1
-            publish.single(topic="v1/devices/me/telemetry", payload='{"value":' + str(slots[0]) +'}', hostname="thingsboard.cloud", auth = {'username':"4Zdi7c8BlbQB7nL1fLpq", 'password':""})
-                
-        elif(slots[1] == plate):
-            slots[1] = "-"
-            available += 1
-            count -= 1
-            publish.single(topic="v1/devices/me/telemetry", payload='{"value":' + str(slots[1]) +'}', hostname="thingsboard.cloud", auth = {'username':"yUYqesVqB0GBThmr1U4h", 'password':""})
-             
-        elif(slots[2] == plate):
-            slots[2] = "-"
-            available += 1
-            count -= 1
-            publish.single(topic="v1/devices/me/telemetry", payload='{"value":' + str(slots[2]) +'}', hostname="thingsboard.cloud", auth = {'username':"nXNiSJDI52nOQqiTy6F0", 'password':""})
         
-        elif(int(b2status) == 1 and slots[2] == "-"):
+        if(int(b2status) == 1 and slots[2] == "-" and plate != slots[0] and plate != slots[1]):
             slots[2] = plate
             display.lcd_display_string("Slot:B1", 1)
             available -= 1
             count += 1
             display.lcd_display_string(plate, 2)
-            publish.single(topic="v1/devices/me/telemetry", payload='{"value":' + str(plate) +'}', hostname="thingsboard.cloud", auth = {'username':"nXNiSJDI52nOQqiTy6F0", 'password':""})
-        
-	elif(slots[0] != "-"):
-	    booked = slots[0]
-	    if(booked[:-1] == plate):
-	        display.lcd_display_string("Slot:A1", 1)
-		count += 1
-		display.lcd_display_string(plate, 2)
-		publish.single(topic="v1/devices/me/telemetry", payload='{"value":' + str(plate) +'}', hostname="thingsboard.cloud", auth = {'username':"4Zdi7c8BlbQB7nL1fLpq", 'password':""})
-		
-	elif(slots[1] != "-"):
-	    booked = slots[1]
-	    if(booked[:-1] == plate):
-	        display.lcd_display_string("Slot:A2", 1)
-		count += 1
-		display.lcd_display_string(plate, 2)
-		publish.single(topic="v1/devices/me/telemetry", payload='{"value":' + str(plate) +'}', hostname="thingsboard.cloud", auth = {'username':"yUYqesVqB0GBThmr1U4h", 'password':""})   
-		   
-
-        elif(slots[0] == "-" and slots[1] =="-"):
-            num = r.randint(0,1)
-            if(num == 0):
-                slots[0] = plate
-                display.lcd_display_string("Slot:A1", 1)
-                publish.single(topic="v1/devices/me/telemetry", payload='{"value":' + str(plate) +'}', hostname="thingsboard.cloud", auth = {'username':"4Zdi7c8BlbQB7nL1fLpq", 'password':""})
-            else:
-                slots[1] = plate
-                display.lcd_display_string("Slot:A2", 1)
-                publish.single(topic="v1/devices/me/telemetry", payload='{"value":' + str(plate) +'}', hostname="thingsboard.cloud", auth = {'username':"yUYqesVqB0GBThmr1U4h", 'password':""})
-            available -= 1
-            count += 1
-            display.lcd_display_string(plate, 2)
-        
         elif(slots[0] == "-"):
-            slots[0] = plate
-            display.lcd_display_string("Slot:A1", 1)
-            available -= 1
-            count += 1
-            display.lcd_display_string(plate, 2)
-            publish.single(topic="v1/devices/me/telemetry", payload='{"value":' + str(plate) +'}', hostname="thingsboard.cloud", auth = {'username':"4Zdi7c8BlbQB7nL1fLpq", 'password':""})
-        
+            come(plate)
         elif(slots[1] == "-"):
-            slots[1] = plate
-            display.lcd_display_string("Slot:A2", 1)
-            available -= 1
-            count += 1
-            display.lcd_display_string(plate, 2)
-            publish.single(topic="v1/devices/me/telemetry", payload='{"value":' + str(plate) +'}', hostname="thingsboard.cloud", auth = {'username':"yUYqesVqB0GBThmr1U4h", 'password':""})
+            come(plate)
+        else:
+            leaved(plate)
         
-        publish.single(topic="v1/devices/me/telemetry", payload='{"value":' + str(available) +'}', hostname="thingsboard.cloud", auth = {'username':"l2W9Yt6qDEhBoTVj1FVB", 'password':""})
-        publish.single(topic="v1/devices/me/telemetry", payload='{"value":' + str(count) +'}', hostname="thingsboard.cloud", auth = {'username':"xHyIFxSBeq87pbF3671m", 'password':""})
         print(str(count))
         arduino.write(b"1")
         time.sleep(3)
         arduino.write(b"2")
         time.sleep(1)
         display.lcd_clear()
-        action = "0"
+        action = 0
+        
+        dbConn = MySQLdb.connect("localhost","pi","","parkingSystem_db") or dle("Could not connect to database") 
+        with dbConn:
+            cursor = dbConn.cursor()
+            cursor.execute("INSERT INTO record (Counts, Availables, SlotA1, SlotA2, SlotB1) VALUES ('%s','%s','%s','%s','%s')" %(count, available, slots[0], slots[1], slots[2])) 
+            dbConn.commit()
+            cursor.close()
+        
+    publish.single(topic="v1/devices/me/telemetry", payload='{"value":' + str(slots[2]) +'}', hostname="thingsboard.cloud", auth = {'username':"nXNiSJDI52nOQqiTy6F0", 'password':""})
+    publish.single(topic="v1/devices/me/telemetry", payload='{"value":' + str(slots[0]) +'}', hostname="thingsboard.cloud", auth = {'username':"4Zdi7c8BlbQB7nL1fLpq", 'password':""})
+    publish.single(topic="v1/devices/me/telemetry", payload='{"status":' + str(s1status) +'}', hostname="thingsboard.cloud", auth = {'username':"4Zdi7c8BlbQB7nL1fLpq", 'password':""})
+    publish.single(topic="v1/devices/me/telemetry", payload='{"value":' + str(slots[1]) +'}', hostname="thingsboard.cloud", auth = {'username':"yUYqesVqB0GBThmr1U4h", 'password':""})
+    publish.single(topic="v1/devices/me/telemetry", payload='{"status":' + str(s2status) +'}', hostname="thingsboard.cloud", auth = {'username':"yUYqesVqB0GBThmr1U4h", 'password':""})
+    publish.single(topic="v1/devices/me/telemetry", payload='{"value":' + str(available) +'}', hostname="thingsboard.cloud", auth = {'username':"l2W9Yt6qDEhBoTVj1FVB", 'password':""})
+    publish.single(topic="v1/devices/me/telemetry", payload='{"value":' + str(count) +'}', hostname="thingsboard.cloud", auth = {'username':"xHyIFxSBeq87pbF3671m", 'password':""})
     display.lcd_display_string("Available:" + str(available), 1)
-    if(count >= 2 and emailSend == False):
+    if(count >= 1 and emailSend == False):
         server.sendmail('iotsmartparking2021@gmail.com', 'iotsmartparking2021@gmail.com',"Alert!!! Open Basement 2!!!")
         emailSend = True
         
-    if(count < 2):
+    if(count < 1):
         emailSend = False
+      
+    
+        
+        
+        
+
+            
---
+++ Sabrina_CarParkSystem/IoT__Launcher/system.py
@@ -0,0 +1,59 @@
+from flask import Flask, render_template, request
+import MySQLdb
+app = Flask(__name__, template_folder="templates")
+available = 3
+count = 0
+slots = ["-", "-","-"]
+
+
+@app.route("/")
+def index():
+    dbConn = MySQLdb.connect("localhost","pi","","parkingSystem_db") or dle("Could not connect to database") 
+    print(dbConn)
+    with dbConn:
+        cursor = dbConn.cursor()
+        cursor.execute("SELECT * FROM record")
+        dbConn.commit()
+        for Id, Counts, Availables, SlotA1, SlotA2, SlotB1 in cursor:
+            count = int(Counts)
+            available = int(Availables)
+            slots[0] = SlotA1
+            slots[1] = SlotA2
+            slots[2] = SlotB1
+        cursor.close()
+        
+    displaySlot = []
+    if(slots[0] == "-"):
+        displaySlot.append("A1")
+    if(slots[1] == "-"):
+        displaySlot.append("A2")
+    return render_template('index.html', available=available, count=count, displaySlot=displaySlot)
+
+@app.route("/", methods=['POST'])
+def getBooking():
+    name = request.form['name']
+    phone = request.form['phone']
+    slotNum = request.form['slotNum']
+    plate = request.form['plate']
+    if(slotNum.upper() == "A1"):
+        slots[0] = plate + "@"
+    elif(slotNum.upper() == "A2"):
+        slots[1] = plate + "@"
+    global count, available
+    count += 1
+    available -= 1
+    
+    dbConn = MySQLdb.connect("localhost","pi","","parkingSystem_db") or dle("Could not connect to database") 
+    print(dbConn)
+    with dbConn:
+        cursor = dbConn.cursor()
+        cursor.execute("INSERT INTO record (Counts, Availables, SlotA1, SlotA2, SlotB1) VALUES ('%s','%s','%s','%s','%s')" %(count, available, slots[0], slots[1], slots[2])) 
+        dbConn.commit()
+        cursor.close()
+    
+    return render_template('bookInfo.html', name=name, phone=phone, slotNum=slotNum, plate=plate)
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=8080) 
+
+
